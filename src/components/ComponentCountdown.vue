631697baf8e5e48dc42784c5f2edd49479182f27
Fabio Teixeira <fabio.teixeira@toogas.com>
2022-09-07 21:58:59 +0100

    v2

--- src/components/ComponentCountdown.vue
+++ src/components/ComponentCountdown.vue
@@ -1,11 +1,10 @@
 <template>
   <main>
     <div class="countdown-block">
-        <MyCountdown deadline="2022-09-24 00:00:00" labelColor="orange" :labels="{days: 'Dias',hours: 'Horas',minutes: 'Minutos',seconds: 'Segundos'}"></MyCountdown>
+        <MyCountdown deadline="2022-09-24 00:00:00" labelColor="gray" mainColor="white" :labels="{days: 'Dias',hours: 'Horas',minutes: 'Minutos',seconds: 'Segundos'}"></MyCountdown>
     </div>
     <div id="Content" class="wrapper">
         <div class="countdown styled"></div>
-        <h3 class="intro">24 de Setembro de 2022</h3>
     </div>
   </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,5 +5,9 @@ import store from './store'
 
 import './assets/countdown_page.css'
 import Notifications from '@kyvg/vue3-notification'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(Notifications).use(store).use(router).mount('#app')
+
+createApp(App).use(Notifications).use(store).use(router).use(BootstrapVue3).mount('#app')
